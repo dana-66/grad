b8dfb8af644b352116034d951051e886de1fc6c1
Dana Tarik <88723929+dana-66@users.noreply.github.com>
2025-02-01 14:21:05 +0200

    Update textCard.jsx

--- resources/js/components/textCard.jsx
+++ resources/js/components/textCard.jsx
@@ -1,40 +1,48 @@
-// import React from 'react';
-import PropTypes from "prop-types";
-// import Divider from '../components/Divider';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Modal from './Modal';
 
-const TextCard = ({
-    heading,
-    textContent,
-    backgroundImage,
-    fontColor,
-    backgroundColor,
-    width,
-    height,
-}) => {
+const TextCard = ({ className, heading, textContent, backgroundImage, fontColor, backgroundColor, width, height, isClickable = true, videoUrl }) => {
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const handleCardClick = () => {
+        if (isClickable) {
+            setModalOpen(true);
+        }
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
+
     const cardStyle = {
-        backgroundColor: backgroundColor || "#071754",
-        backgroundImage: backgroundImage ? `url(${backgroundImage})` : "none",
-        color: fontColor || "#000",
-        padding: "2rem 1.4rem",
-        borderRadius: "1rem",
+        backgroundColor: backgroundColor || '#071754',
+        backgroundImage: backgroundImage ? `url(${backgroundImage})` : 'none',
+        color: fontColor || '#000',
+        padding: '2rem 1.4rem',
+        borderRadius: '1rem',
         boxShadow: `#000 0px 1px 4px`,
         width: width,
-        height: height, //this is temporary
-        textAlign: "-webkit-left;",
+        height: height,
+        cursor: isClickable ? 'pointer' : 'default',
     };
 
     return (
-        <div className="textCard" style={cardStyle}>
-            <h2>{heading}</h2>
-            {/* <Divider color="#eff4ef" thickness="1px" margin="4px" rounded={true} /> */}
-
-            <p>{textContent}</p>
-        </div>
+        <>
+            <div className={`text-card ${className}`} style={cardStyle} onClick={handleCardClick}>
+                <h2>{heading}</h2>
+                {/* <Divider color="#eff4ef" thickness="1px" margin="4px" rounded={true} /> */}
+               
+                <p>{textContent}</p>
+            </div>
+            {isModalOpen && <Modal onClose={handleCloseModal} videoUrl={videoUrl} />}
+        </>
     );
 };
 
 //Prototypes for the card
 TextCard.propTypes = {
+    className: PropTypes.string,
     heading: PropTypes.string.isRequired,
     textContent: PropTypes.string.isRequired,
     backgroundImage: PropTypes.string,
@@ -42,6 +50,8 @@ TextCard.propTypes = {
     backgroundColor: PropTypes.string,
     width: PropTypes.string,
     height: PropTypes.string,
-};
+    isClickable: PropTypes.bool,
+    videoUrl: PropTypes.string.isRequired,
+}
 
 export default TextCard;
